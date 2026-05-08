c79ce8bc3f5125c3643016c3e14d59b65c102a2c
Jeremy Britt <jeremy.britt@oddball.io>
2026-05-08 10:10:46 -0400

    down a dir

--- devops-challenge-legacy/terraform/modules/ecs_container_definition/outputs.tf
+++
@@ -1,6 +0,0 @@
-output "json_map" {
-  description = "JSON string encoded container definitions for use with other terraform resources such as aws_ecs_task_definition"
-  value       = local.json_map
-}
-
-
